5a0d4de0a5a10160142678d473db85ebb8648fb1
Matt Hurd <matthewdhurd96@gmail.com>
2016-09-30 00:04:21 +0000

    Added favicon and footer, fixed 404

--- locs42/views.py
+++ locs42/views.py
@@ -3,10 +3,13 @@ import datetime
 from cluster import get_cluster
 from django.http import HttpResponse
 
-# cluster = get_cluster()
 def gen_cluster(request, number="1"):
     cluster = get_cluster(number)
     return render(request, 'main.html', {'cluster' : cluster, 'number' : number})
+
+def beta(request):
+    cluster = get_cluster('1')
+    return render(request, 'beta.html', {'cluster' : cluster, 'number' : '1'})
     
 def error404(request):
     return HttpResponse(render(request, '404.html'), status=404)
